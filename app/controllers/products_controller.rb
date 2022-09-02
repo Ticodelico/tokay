f24d540b98b322a4c8478a739fc49f8cd0092c1f
Lilliam Arce <lilliamarce@MacBook-Pro-de-Lilliam.local>
2022-09-02 16:37:33 -0600

    Project creation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,26 @@
 class ProductsController < ApplicationController
-    def index
-       @products = Product.all
+  def index
+    @products = Product.all
+  end
+  def show
+    @product = Product.find(params[:id])
+  end
+  def new
+    @product = Product.new
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
-       @product = Product.find(params[:id])
-    end
+  private
 
-    def new
-      @product = Product.new
-    end
-
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
 end
