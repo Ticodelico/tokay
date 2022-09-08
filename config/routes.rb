83d02c69f0e048a0f2dc9a0273966c439fef5794
Johnny Mendez <ticodelico@MacBook-Pro-de-Lilliam.local>
2022-09-08 01:11:25 -0600

    Cards for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create], path: '/register', path_names: { new: '/' }
+    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+
+  end
   get 'pages/index'
   get 'pages/faq'
   get 'pages/contact'
@@ -6,6 +12,6 @@ Rails.application.routes.draw do
   get 'pages/blog'
   get 'pages/about'
   resources :categories, except: :show
-  resources :products
+  resources :products, path: '/'
   root "products#index"
 end
