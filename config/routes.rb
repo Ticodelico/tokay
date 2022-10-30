0590b8a1c918527ca066636eca50611b70014652
Johnny Mendez <03jomr1995@gmail.com>
2022-10-30 01:02:24 -0600

    Added admin to header

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Rails.application.routes.draw do
   namespace :authentication, path: '', as: '' do
     resources :users, only: [:new, :create], path: '/register', path_names: { new: '/' }
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
-
   end
   get 'pages/index'
   get 'pages/faq'
